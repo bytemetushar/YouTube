f4218a9cf410dc4f374862eb5938ac349b6f5ce7
IntelliSignal <intellisignal@gmail.com>
2025-02-26 22:38:20 +0530

    new changesssss

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base:"/Youtube-clone"
   // server:{
   //   host: '0.0.0.0',
   //   allowedHosts: true
